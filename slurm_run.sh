584485931a51b342a6afbae50a4f1958440e730e
Guandao Yang <gy46@cornell.edu>
2022-03-30 14:14:25 -0400

    Update slurm script

--- slurm_run.sh
+++ slurm_run.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+source /home/gy46/.bashrc
 cd /home/gy46/a3-leaderboard/scripts
 # /share/apps/anaconda3/2021.05/bin/conda init &
-# source /home/gy46/.bashrc & /share/apps/anaconda3/2021.05/bin/conda activate /home/gy46/anaconda2/envs/ece5545
-export PATH="/home/gy46/anaconda2/envs/ece5545:${PATH}"
+# /share/apps/anaconda3/2021.05/bin/conda activate /home/gy46/anaconda2/envs/ece5545
+export PATH="/usr/local/cuda/bin/:/home/gy46/anaconda2/envs/ece5545/bin/:${PATH}"
 python run_all_repos.py
