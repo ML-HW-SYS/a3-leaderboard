17820db9ff8d2fcd442c8f0fec339aa1301e4408
Yash Akhauri <ya255@g2-login-02.coecis.cornell.edu>
2023-03-20 13:27:37 -0400

    2023 changes

--- slurm_run.sh
+++ slurm_run.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-source /home/gy46/.bashrc
-cd /home/gy46/a3-leaderboard/scripts
-# /share/apps/anaconda3/2021.05/bin/conda init &
-# /share/apps/anaconda3/2021.05/bin/conda activate /home/gy46/anaconda2/envs/ece5545
-export PATH="/usr/local/cuda/bin/:/home/gy46/anaconda2/envs/ece5545/bin/:${PATH}"
+source /share/apps/anaconda3/2021.05/etc/profile.d/conda.sh 
+
+conda activate unr
+
+cd /home/ya255/a3-leaderboard/scripts
+# # /share/apps/anaconda3/2021.05/bin/conda init &
+# # /share/apps/anaconda3/2021.05/bin/conda activate /home/gy46/anaconda2/envs/ece5545
+# export PATH="/usr/local/cuda/bin/:/home/gy46/anaconda2/envs/ece5545/bin/:${PATH}"
 python run_all_repos.py
