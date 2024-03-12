437b12ced994a63457edbf8818e812c7179a1d1b
Ahmed AbouElhamayed <afa55@cornell.edu>
2024-03-12 15:41:10 -0400

    Update to run anywhere

--- slurm_run.sh
+++ slurm_run.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-source /share/apps/anaconda3/2021.05/etc/profile.d/conda.sh 
+export PATH="/usr/local/cuda/bin:$PATH"
 
-conda activate unr
+cd /home/afa55/a3-leaderboard
 
-cd /home/ya255/projects/a3-leaderboard/scripts
+source env/bin/activate
+
 # # /share/apps/anaconda3/2021.05/bin/conda init &
 # # /share/apps/anaconda3/2021.05/bin/conda activate /home/gy46/anaconda2/envs/ece5545
 # export PATH="/usr/local/cuda/bin/:/home/gy46/anaconda2/envs/ece5545/bin/:${PATH}"
-python run_all_repos.py
+export GH_TOKEN=YOUR_TOKEN_HERE
+python scripts/run_all_repos.py
